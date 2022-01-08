23b45fca97edd086ab89e218a1411064c7fca706
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2022-01-08 19:30:53 +0530

    package created

---
+++ index.js
@@ -0,0 +1,15 @@
+const axios = require("axios").default;
+const say = require("say");
+const chalkAnimation = require("chalk-animation");
+const joke = async () => {
+  let anim = chalkAnimation.rainbow("\n Welcome To Joke Teller \n");
+  await new Promise((res) => setTimeout(res, 1500));
+  anim.stop();
+  let url = "https://api.chucknorris.io/jokes/random";
+
+  await axios.get(url).then((response) => {
+    say.speak(response.data.value);
+  });
+};
+
+joke();
