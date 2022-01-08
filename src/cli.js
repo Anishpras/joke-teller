790227a891ba084dd45530e1b6aaace5839a87d4
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2022-01-08 19:57:26 +0530

    updated package

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const say = require("say");
 const chalkAnimation = require("chalk-animation");
-const joke = async () => {
+export async function cli() {
   let anim = chalkAnimation.rainbow("\n Welcome To Joke Teller \n");
   await new Promise((res) => setTimeout(res, 1500));
   anim.stop();
@@ -10,6 +10,4 @@ const joke = async () => {
   await axios.get(url).then((response) => {
     say.speak(response.data.value);
   });
-};
-
-joke();
+}
